237990addbeecac8fd5bf3d8cd130f2a01256dca
Anastasia <cookiedemons@outlook.com>
2026-06-05 23:16:04 -0400

    feat: initializing nix dev environments

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "ao3-wrapped dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            # Python backend
+            python312
+            python312Packages.pip
+            python312Packages.virtualenv
+
+            # Node frontend
+            nodejs_22
+            nodePackages.npm
+
+            # Tooling
+            git
+          ];
+
+          shellHook = ''
+            if [ ! -d .venv ]; then
+              python -m venv .venv
+            fi
+            source .venv/bin/activate
+          '';
+        };
+      });
+}
