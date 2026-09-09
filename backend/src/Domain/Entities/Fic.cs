f119a9a72f71eaecd5b0d99078a71ad303b3f0e0
Anastasia <cookiedemons@outlook.com>
2026-09-09 19:38:54 -0400

    feat: adding new fields after feedback

--- backend/src/Domain/Entities/Fic.cs
+++ backend/src/Domain/Entities/Fic.cs
@@ -8,7 +8,8 @@ namespace AO3W.Domain.Entities
     public string Author { get; set; }
     public string Rating { get; set; }
     public string Warning { get; set; }
-    public string Category { get; set; }
+    public string Pairing { get; set; }
+    public string Status { get; set; }
     public string Language { get; set; }
 
     public List<string> Fandoms { get; set; }
@@ -17,11 +18,16 @@ namespace AO3W.Domain.Entities
     public List<string> AdditionalTags { get; set; }
 
     public DateTime Published { get; set; }
+    public DateTime LastUpdated { get; set; }
+
     public int Words { get; set; }
     public int Chapters { get; set; }
+    
     public int Kudos { get; set; }
     public int Hits { get; set; }
     public int Comments { get; set; }
     public int Bookmarks { get; set; }
+
+    public List<string> Collections { get; set; }
   }
 }
